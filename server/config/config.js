fd56cd98708da9b2c45fbae574d67208ef21b33a
Jaime <jaime.escoto@hotmail.com>
2019-09-23 18:20:24 -0600

    Creating enviroment variable for Data Base connection

--- server/config/config.js
+++ server/config/config.js
@@ -12,7 +12,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafestable';
 } else {
-    urlDB = 'mongodb+srv://dev:jted1990@cluster0-z5ixd.mongodb.net/cafe?retryWrites=true&w=majority'
+    urlDB = process.env.MONGODB;
 
 }
 
